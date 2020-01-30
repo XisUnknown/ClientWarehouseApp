da2a2b9855981587477a5a96cb6a75fc56df6f9d
Wojciech Schlesinger <wojciech.schlesinger@gmail.com>
2020-01-30 13:41:24 +0100

    clean up

--- Client-App/src/sample/SQLStatements.java
+++
@@ -1,8 +0,0 @@
-package sample;
-
-public class SQLStatements {
-    private String createChoiceBoxStmt = "SELECT productID,productname FROM product WHERE instock <> 0";
-    private String createAllProductsTableStmt = "SELECT productID,productname,singleprice,bulkprice,instock FROM product WHERE instock <> 0";
-    private String createCoffeeTableStmt = "SELECT productID,productname,singleprice,bulkprice,instock FROM product WHERE category LIKE ('Coffee') AND instock <> 0";
-
-}
